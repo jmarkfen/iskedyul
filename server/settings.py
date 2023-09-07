4b08fb791ad16d9670ec34b5ba8fbc7bb4c3d21d
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-09-07 19:39:09 +0800

    fix typo in django-reload-browser middleware

--- server/settings.py
+++ server/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django_browser_reload.middleware.BrowserReloadMiddleWare",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "server.urls"
