8fad591623d54baa0c4b255a800c6d379ae7a18c
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-09-07 21:50:06 +0800

    create views to create and save timetables

--- iskedyul/views.py
+++ iskedyul/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from iskedyul.forms import TimetableForm
 
 # Create your views here.
+
+def create_timetable(request):
+    template_name = "iskedyul/pages/create_timetable.html"
+    context = {
+        "form_action": reverse(save_timetable),
+        "next_url": reverse(create_timetable),
+    }
+    return render(request, template_name, context)
+
+def save_timetable(request):
+    form = TimetableForm(request.POST)
+    if form.is_valid():
+        form.save()
+    return redirect(request.POST.get("next_url"))
