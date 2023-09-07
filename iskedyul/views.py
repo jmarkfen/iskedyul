b12adde670eb61d6b33dcde68152744305bf8935
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-09-07 22:51:42 +0800

    redirect to timetable list after saving timetable

--- iskedyul/views.py
+++ iskedyul/views.py
@@ -10,7 +10,7 @@ def create_timetable(request):
     template_name = "iskedyul/pages/create_timetable.html"
     context = {
         "form_action": reverse(save_timetable),
-        "next_url": reverse(create_timetable),
+        "next_url": reverse(timetable_list),
     }
     return render(request, template_name, context)
 
