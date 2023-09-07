0fcd8b710371ab7dbafacf8e1b61ebae6d08e2cf
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-09-07 19:48:00 +0800

    create url paths for creating and saving timetables

--- iskedyul/urls.py
+++ iskedyul/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from iskedyul import views
+
 urlpatterns = [
-    
+    path("timetables/new/", views.create_timetable, name="create_timetable"),
+    path("timetables/save/", views.save_timetable, name="save_timetable"),
 ]
