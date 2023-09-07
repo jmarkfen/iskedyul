13570664d175dbd5da767109c94c82a2c6ac8881
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-09-07 19:21:18 +0800

    create model for timetables

--- iskedyul/models.py
+++ iskedyul/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class Timetable(models.Model):
+    """Model definition for Timetable."""
+
+    title = models.CharField(_("title"), max_length=50)
+
+    class Meta:
+        """Meta definition for Timetable."""
+
+        verbose_name = "Timetable"
+        verbose_name_plural = "Timetables"
+
+    def __str__(self):
+        """Unicode representation of Timetable."""
+        return self.title
