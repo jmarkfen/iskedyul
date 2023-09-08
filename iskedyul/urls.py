90d09bb19aa52ef809c2b538e4529ee4354c2f1a
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-09-08 18:22:53 +0800

    add url paths for deleting timetables

--- iskedyul/urls.py
+++ iskedyul/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("timetables/save/", views.save_timetable, name="save_timetable"),
     path("timetables/", views.timetable_list, name="timetable_list"),
     path("timetables/<int:timetable_id>/", views.edit_timetable, name="edit_timetable"),
+    path("timetables/<int:timetable_id>/delete/", views.delete_timetable_dialog, name="delete_timetable_dialog"),
+    path("timetables/delete/", views.delete_timetable, name="delete_timetable"),
 ]
