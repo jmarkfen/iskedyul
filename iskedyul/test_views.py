6cb5bbacb176a9f8365c459789e02461613a57e8
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-09-08 19:05:02 +0800

    write tests for deleting timetable

--- iskedyul/test_views.py
+++ iskedyul/test_views.py
@@ -49,3 +49,28 @@ class TestViews(TestCase):
         response = self.client.get(reverse(views.timetable_list))
         self.assertEqual(response.status_code, 200)
     
+    def test_delete_timetable_dialog(self):
+        """
+        test confirmation dialog for deleting timetable
+        """
+        timetable = Timetable.objects.create(title="Test timetable")
+        args = [
+            timetable.pk,
+        ]
+        response = self.client.get(reverse(views.delete_timetable_dialog, args=args))
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_timetable(self):
+        """
+        test deleting timetable
+        """
+        timetable = Timetable.objects.create(title="Test timetable")
+        form_data = {
+            "id": timetable.pk,
+            "button_choice": "ok",
+            "next_url": reverse(views.timetable_list),
+        }
+        response = self.client.post(reverse(views.delete_timetable), data=form_data)
+        self.assertEqual(response.status_code, 302)  # redirect status: 302 Found
+        # verify the model does not exist in the database
+        self.assertNotIn(timetable, Timetable.objects.all())
