a6370918a5b18d106719fb970ba5bf5a1647191a
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-09-08 15:33:46 +0800

    create url path for editing timetables

--- iskedyul/urls.py
+++ iskedyul/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("timetables/new/", views.create_timetable, name="create_timetable"),
     path("timetables/save/", views.save_timetable, name="save_timetable"),
     path("timetables/", views.timetable_list, name="timetable_list"),
+    path("timetables/<int:timetable_id>/", views.edit_timetable, name="edit_timetable"),
 ]
